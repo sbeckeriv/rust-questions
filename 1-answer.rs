e661e2ece99c80c231250bda1c9bcac82133005f
Stephen Becker IV <github@deathbyescalator.com>
2015-12-28 12:02:26 -0800

    update answer

--- 1-answer.rs
+++ 1-answer.rs
@@ -1,9 +1,10 @@
 use std::io;
 use std::collections::HashSet;
+use std::ascii::AsciiExt;
 
 fn isPangram(s: String) -> bool {
     let alpha: HashSet<u8> = (b'a'..b'z' + 1).collect();
-    let string: HashSet<u8> = s.chars().map(|x| x as u8).collect();
+    let string: HashSet<u8> = s.to_ascii_lowercase().chars().map(|x| x as u8).collect();
     alpha.is_subset(&string)
 }
 
